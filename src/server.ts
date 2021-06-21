c8cedf38996a2c39fed7824cc9de77e0170066d7
George Armando <georgearmando54@gmail.com>
2021-06-21 12:50:13 +0100

    eslint and prettier in action

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/test', (request, response) => {
-  return response.send('Teste Get');
-});
+app.get("/test", (request, response) => response.send("Teste Get"));
 
-app.post('/test-post', (request, response) => {
-  return response.send('Test Post')
-});
+app.post("/test-post", (request, response) => response.send("Test Post"));
 
-app.listen('3000', () => {
-  console.log('Server is running on port 3000...')
+app.listen("3000", () => {
+  console.log("Server is running on port 3000...");
 });
