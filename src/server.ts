0f1480a2cb80ee95cb6cb0c5c4010eceb9d4a1c5
George Armando <georgearmando54@gmail.com>
2021-06-22 20:34:02 +0100

    indent the file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express from "express";
+import 'reflect-metadata';
+import express from 'express';
+
+import './database';
 
 const app = express();
 
-app.get("/test", (request, response) => response.send("Teste Get"));
+app.get('/test', (request, response) => response.send('Teste Get'));
 
-app.post("/test-post", (request, response) => response.send("Test Post"));
+app.post('/test-post', (request, response) => response.send('Test Post'));
 
-app.listen("3000", () => {
-  console.log("Server is running on port 3000...");
+app.listen('3000', () => {
+  console.log('Server is running on port 3000...');
 });
