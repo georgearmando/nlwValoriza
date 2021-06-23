c737284f97a9a5d4a3141e3013e5ed8d0f483178
George Armando <georgearmando54@gmail.com>
2021-06-23 17:23:19 +0100

    Add tag creation service

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from 'typeorm';
+
+import { Tag } from '../entities/Tag';
+import { TagRepository } from '../repositories/TagRepository';
+
+class CreateTagService {
+  async execute(name: string): Promise<Tag> {
+    const tagRepository = getCustomRepository(TagRepository);
+
+    if (!name) {
+      throw new Error('Incorrect name');
+    }
+
+    const tagAlreadyExists = await tagRepository.findOne({
+      name,
+    });
+
+    if (tagAlreadyExists) {
+      throw new Error('Tag already exists');
+    }
+
+    const tag = tagRepository.create({
+      name,
+    });
+
+    await tagRepository.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService };
