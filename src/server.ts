a8cc7c74aae36f10be96077a1e844cc1bb8278e0
George Armando <georgearmando54@gmail.com>
2021-06-23 17:41:30 +0100

    Refactors middleware errors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
 import './database';
+import { AppError } from './errors/AppErrors';
 import { router } from './routes';
 
 const app = express();
@@ -12,15 +13,16 @@ app.use(router);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-      return response.status(400).json({
-        error: err.message,
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
       });
     }
 
     return response.status(500).json({
       status: 'error',
-      message: 'Internal server error',
+      message: `Internal server error - ${err.message}`,
     });
   },
 );
