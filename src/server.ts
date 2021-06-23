14fbe8e7fceaf1933305837aa667b8e8e101ac51
George Armando <georgearmando54@gmail.com>
2021-06-23 17:20:15 +0100

    Add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import './database';
 import { router } from './routes';
@@ -8,6 +9,21 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen('3000', () => {
   console.log('Server is running on port 3000...');
