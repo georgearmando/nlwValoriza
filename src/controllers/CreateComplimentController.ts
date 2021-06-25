07f749740ab402e4c59893f3eaab344f421b2a75
George Armando <georgearmando54@gmail.com>
2021-06-25 14:44:12 +0100

    get the id from the user of the request

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -4,12 +4,13 @@ import { CreateComplimentService } from '../services/CreateComplimentService';
 
 class CreateComplimentController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { user_sender, user_receiver, tag_id, message } = request.body;
+    const { user_receiver, tag_id, message } = request.body;
+    const { id } = request.user;
 
     const createComplimentService = new CreateComplimentService();
 
     const compliment = await createComplimentService.execute({
-      user_sender,
+      user_sender: id,
       user_receiver,
       tag_id,
       message,
