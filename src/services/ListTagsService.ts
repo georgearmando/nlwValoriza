d75dfba586ef50a57e51acf72ed0e071be9e886d
George Armando <georgearmando54@gmail.com>
2021-06-25 14:53:25 +0100

    Create tag listing service

---
+++ src/services/ListTagsService.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { classToPlain } from 'class-transformer';
+import { getCustomRepository } from 'typeorm';
+
+import { TagRepository } from '../repositories/TagRepository';
+
+class ListTagsService {
+  async execute() {
+    const tagRepository = getCustomRepository(TagRepository);
+
+    const tags = await tagRepository.find();
+
+    return classToPlain(tags);
+  }
+}
+
+export { ListTagsService };
