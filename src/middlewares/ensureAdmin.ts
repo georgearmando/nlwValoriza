f6957d8524760cb4f9f5529c19f4ec7ffc430a2e
George Armando <georgearmando54@gmail.com>
2021-06-25 14:51:41 +0100

    Get the user id passed in the request

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,17 +1,24 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Request, Response, NextFunction } from 'express';
+import { getCustomRepository } from 'typeorm';
 
 import { AppError } from '../errors/AppErrors';
+import { UsersRepository } from '../repositories/UserRepository';
 
-export function ensureAdmin(
+export async function ensureAdmin(
   request: Request,
   response: Response,
   next: NextFunction,
 ) {
-  const admin = true;
+  const { id } = request.user;
+
+  const usersRepository = getCustomRepository(UsersRepository);
+
+  const { admin } = await usersRepository.findOne(id);
 
   if (!admin) {
     throw new AppError('Unauthorized', 401);
   }
 
-  next();
+  return next();
 }
